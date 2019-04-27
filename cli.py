8e88cd17870f8f0f988b81b6e41bc0a56c9702c1
Ben Randerson <ben.m.randerson@gmail.com>
2019-04-28 00:40:42 +0100

    read from input data file

--- cli.py
+++ cli.py
@@ -4,8 +4,11 @@
 import sys
 import click
 import numpy as np
+import pandas as pd
+from tabulate import tabulate
 
 from wall import wall_thicknesses
+from wall.plot import plot_wall_thicknesses
 
 
 @click.command()
@@ -14,29 +17,40 @@ def main(args=None):
     P_d = 380e5
     h_ref = 24
     rho_sw = 1025
-    rho_c = 10
+    rho_c = 100
 
-    x = np.array([0, 1])
-    location = np.array(["seabed", "seabed"])
-    D_o = np.array([0.2731, 0.2731])
-    h = np.array([200, 50])
-    sigma_y = np.array([427e6, 427e6])
-    t_corr = np.array([0.001, 0.001])
-    f_tol = np.array([0.125, 0.125])
-    E = np.array([207e9, 207e9])
-    v = np.array([0.3, 0.3])
-    f_0 = np.array([0.01, 0.01])
+    i = pd.read_csv("InputData.csv")
+
+    x = i["KP"].values
+    location = i["Location"].values
+    D_o = i["D_o"].values
+    h = i["h"].values
+    sigma_y = i["sigma_y"].values
+    t_corr = i["t_corr"].values
+    f_tol = i["f_tol"].values
+    E = i["E"].values
+    v = i["v"].values
+    f_0 = i["f_0"].values
 
     wts = wall_thicknesses(
-        P_d, h_ref, rho_c, x, location, D_o, h, sigma_y, t_corr, f_tol, E, v, f_0
+        P_d,
+        h_ref,
+        rho_c,
+        x,
+        location,
+        D_o,
+        h,
+        sigma_y,
+        t_corr,
+        f_tol,
+        E,
+        v,
+        f_0,
+        rho_sw,
     )
-
-    from tabulate import tabulate
 
     headers = ["KP [m]", "Hoop [m]", "Collapse [m]", "Buckle [m]", "Required [m]"]
     print(tabulate(np.transpose(wts), headers, tablefmt="psql"))
-
-    from plot import plot_wall_thicknesses
 
     plot = plot_wall_thicknesses(wts)
     plot.savefig("wall_plot.png")
